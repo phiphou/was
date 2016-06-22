08958cc121e796ce6831f3678f7e0a57ebf28cc4
Phiphou <phiphou@phiphou.com>
2016-06-22 02:36:41 +0200

    update dependencies

--- src/vendors.ts
+++ src/vendors.ts
@@ -9,13 +9,15 @@ import '@angular/router-deprecated';
 // RxJS
 // import 'rxjs';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/delay';
+
 
 import 'script!../node_modules/jquery/dist/jquery.min.js';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 // import 'jquery';
 import 'foundation-sites/dist/plugins/foundation.core';
-import 'foundation-sites/dist/plugins/foundation.util.mediaQuery';
+// import 'foundation-sites/dist/plugins/foundation.util.mediaQuery';
 import 'foundation-sites/dist/plugins/foundation.util.triggers';
 
 import '@angular2-material/input';
