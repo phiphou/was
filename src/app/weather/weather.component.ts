6886717d528b214c652383a13ae19fa19b06675c
Phiphou <phiphou@phiphou.com>
2016-07-03 04:08:27 +0200

    remove MdSlideToggle

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -8,14 +8,13 @@ import {DateConvertPipe} from '../shared/pipes/dateConvert.pipe';
 import {TempConvertPipe} from '../shared/pipes/tempConvert.pipe';
 import {Settings} from '../shared/settings';
 import {WeatherInputComponent} from './weather-input.component';
-import {MdSlideToggle} from '@angular2-material/slide-toggle';
 import {TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
 
 @Component({
   selector: 'weather',
   providers: [HTTP_PROVIDERS, Settings, WeatherService],
   pipes: [DateConvertPipe, TempConvertPipe, TranslatePipe],
-  directives: [NgFor, WeatherInputComponent, MdSlideToggle],
+  directives: [NgFor, WeatherInputComponent],
   template: require('./weather.template.html')
 })
 
