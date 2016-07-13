984343b6a0d97a1684a7ecca104af4db37c340e9
Phiphou <phiphou@phiphou.com>
2016-07-13 21:53:17 +0200

    update test.sh

--- config/test.sh
+++ config/test.sh
@@ -1,3 +1,3 @@
-./node_modules/protractor/bin/webdriver-manager update
-http-server dist -a 127.0.0.1 -s & ./node_modules/protractor/bin/webdriver-manager start &
-./node_modules/protractor/bin/protractor ./config/protractor.conf.js
+webdriver-manager update
+http-server dist -a 127.0.0.1 -s & webdriver-manager start &
+protractor ./config/protractor.conf.js
